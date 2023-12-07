00828671cec86479304be164b745ecbd7159b385
Kalina <olmifrant@gmail.com>
2023-12-07 12:30:02 +0100

    HomeWork 07.12.23

---
+++ classwork/src/lesson_002/homeWorkTwoPartOne.java
@@ -0,0 +1,14 @@
+package lesson_002;
+
+public class homeWorkTwoPartOne {
+
+    public static void main(String[] args) {
+
+        int a = 51;
+        int b = 27;
+
+        double averageSum = (a + b) / 2;
+        System.out.println(averageSum);
+
+    }
+}
---
+++ classwork/src/lesson_002/homeWorkTwoPartThree.java
@@ -0,0 +1,36 @@
+package lesson_002;
+
+public class homeWorkTwoPartThree {
+
+    public static void main(String[] args) {
+
+
+        System.out.println();
+        System.out.println("Deposit Calculator");
+
+        double summ = Double.parseDouble(args[0]);
+        double per = Double.parseDouble(args[1]);
+        double dur = Double.parseDouble(args[2]);
+
+
+        System.out.println();
+        System.out.println("Cумма вклада " + summ + " UAH");
+        System.out.println("Процентная ставка " + per + " % годовых");
+        System.out.println("Cрок вклада " + dur + " лет");
+
+
+        System.out.println();
+        double balanse = 0;
+        for (int year = 1; year <= dur; year++) {
+            balanse = summ * Math.pow(1 + (per/100), year);
+            System.out.println("Баланс за " + year + " год " + "составляет " + balanse + " UAH" + " проценты " +
+                    (balanse - summ) + " UAH");
+
+        }
+
+
+    }
+}
+
+
+
