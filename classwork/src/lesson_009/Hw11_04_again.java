54ac3ff15d8c03024bae2a815915c1e92e456c0f
Kalina <olmifrant@gmail.com>
2024-01-09 17:37:05 +0100

    HomeWork_11

---
+++ classwork/src/lesson_009/Hw11_04_again.java
@@ -0,0 +1,62 @@
+package lesson_009;
+
+import java.util.*;
+
+public class Hw11_04_again {
+
+
+    public static void main(String[] args) {
+
+        ArrayList<String> list1 = new ArrayList<>();
+
+        int size = 100;
+        for (int i = 0; i < size ; i++) {
+            int n = (int) ((Math.random() * 5) +1);
+            switch (n){
+                case 1: list1.add("Кот"); break;
+                case 2: list1.add("Пес"); break;
+                case 3: list1.add("Лис"); break;
+                case 4: list1.add("Ёж"); break;
+                case 5: list1.add("Гусь"); break;
+            }
+        }
+
+        System.out.println(findOccurance(list1));
+
+    }
+
+    public static HashMap findOccurance(List list) {
+
+        Set<String> set = new HashSet<>(list);
+        ArrayList<String> setUni = new ArrayList<>(set);
+
+        String[] uniqList = new String[set.size()];
+        for (int i = 0; i < uniqList.length; i++) {
+            uniqList[i] = setUni.get(i);
+        }
+
+        String[] any = new String[list.size()];
+        for (int i = 0; i < any.length; i++) {
+            any[i] = list.get(i).toString();
+        }
+
+        HashMap<Integer , String> map = new HashMap<>();
+
+        for (int i = 0; i < uniqList.length; i++) {
+            int count = 0;
+            for (int j = 0; j < any.length; j++) {
+                if (uniqList[i].equals(any[j])) {
+                    count++;
+                }
+            }
+            //System.out.println("Значение " + uniqList[i] + " повторяется " + count + " раз");
+            map.put(count, uniqList[i] );
+        }
+        return map;
+    }
+}
+
+
+
+
+
