f09525f3f52c264bfe4ec400daa72192308cc40b
Kalina <olmifrant@gmail.com>
2024-01-18 14:12:01 +0100

    HomeWork_14

--- classwork/src/lesson_011/MyArraySizeException.java
+++ classwork/src/lesson_011/MyArraySizeException.java
@@ -2,6 +2,7 @@ package lesson_011;
 
 public class MyArraySizeException extends CustomException {
 
+
     MyArraySizeException() {
         super("Размерность массива должна быть [4 x 4]\n");
     }
